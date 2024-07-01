8b6d1c4bc75987074b1750bc6acc844b7b5ccc62
gayeoneee <164208099+gayeoneee@users.noreply.github.com>
2024-07-01 15:05:12 +0900

    accounts_ver3
    회원가입(이메일 인증) 제외 완료

---
+++ 2024-Herethon-3/accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from .models import CustomUsers
+from .forms import SignupForm
+
+def temp(request):
+    return render(request, 'temp.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:temp')
+        else:
+            return render(request, 'signup.html', {'form': form})
+    else:
+        form = SignupForm()
+        return render(request, 'signup.html', {'form': form})
